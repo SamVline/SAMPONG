365700c11a19492e934fbf9de1e0279ab5dafbe9
Sam <vlinevlog@gmail.com>
2023-02-07 08:40:12 +0100

    agrego archivo de dependencias

--- pong.py
+++ pong.py
@@ -6,14 +6,15 @@ ANCHO = 640
 ALTO = 480
 
 CBLANCO = (255, 255, 180)
-#### Ejercicio 2, añadir color
-GRAY = (41, 81, 41)
-#####
+AZUL = (10, 100, 100)
+VERDE = (100, 250, 200)
+
+
 ANCHO_PALETA = 10
 ALTO_PALETA = 40
 MARGEN_LATERAL = 40
 
-TAM_PELOTA = 5
+TAM_PELOTA = 10
 
 class Jugador(pygame.Rect):
     def __init__(self,pos_x, pos_y):
@@ -26,38 +27,31 @@ class Pelota(pygame.Rect):
     def __init__(self, x, y):
         self.rectangulo = pygame.Rect(x, y, TAM_PELOTA, TAM_PELOTA)
     def pintame(self, pantalla):
-        pygame.draw.rect(pantalla, CBLANCO, self.rectangulo)
+        pygame.draw.rect(pantalla, (250, 250, 0), self.rectangulo)
 
 class Pong:
-    
-
     def __init__(self):
         print("Construyendo un objeto de la clase Pong")
         pygame.init()
         self.pantalla = pygame.display.set_mode((ANCHO, ALTO))
     ################## AÑADIMOS EL COLOR TRAS DEFINIR EL TAMAÑO
-        pygame.Surface.fill(self.pantalla, GRAY)
-    ##################
+        pygame.Surface.fill(self.pantalla, AZUL)
         
         pos_y = (ALTO-ALTO_PALETA)/2
         pos_x_2 = ANCHO-MARGEN_LATERAL-ANCHO_PALETA
-
-       
-    ##  PARA HACER LINEAS CONTIGUAS
- 
-      ##ESTA ESTA BIEN##  pygame.draw.aaline(self.pantalla, (0,0,255), [(ANCHO/2)-1, 0], [(ANCHO/2)-1, ALTO])
-    ##    pygame.draw.lines(self.pantalla, (0.200,0), True, [(ANCHO/2)-1, 0], [(ANCHO/2)-1, ALTO])
-    ##linea de separación
-        pygame.draw.lines(self.pantalla, (220,0,0), True, [((ANCHO/2)-1,ALTO), ((ANCHO/2)-1,0)], 4)
+    
     ##LINEAS DE CAMPO
-            ##CENTRAL SUPERIOR
-        pygame.draw.lines(self.pantalla, (180,0,0), True, [(ANCHO,40),(-ANCHO,40)], 2)   
-            ##CENTRAL INFERIOR
-        pygame.draw.lines(self.pantalla, (180,0,0), True, [(ANCHO,ALTO-40),(-ANCHO,ALTO-40)], 2)  
-
-       # pygame.draw.lines(self.pantalla, (180,0,0), True, [(ANCHO,20),(-ANCHO,40)], 2)  
-
-
+    ##CENTRAL SUPERIOR
+        pygame.draw.lines(self.pantalla, (VERDE), True, [(ANCHO,40),(-ANCHO,40)], 2)   
+    ##CENTRAL INFERIOR
+        pygame.draw.lines(self.pantalla, (VERDE), True, [(ANCHO,ALTO-40),(-ANCHO,ALTO-40)], 2)  
+    
+##RED DE SEPARACION
+        for i in range(0, ALTO, ALTO//20):
+            if i % 2 == 1:
+                    continue
+            pygame.draw.line(self.pantalla, (VERDE), [(ANCHO//2)-1, i], [(ANCHO//2)-1, i + 15], 2)
+#############################
         self.jugador1 = Jugador(MARGEN_LATERAL,pos_y)
         self.jugador2 = Jugador(pos_x_2, pos_y)
 
@@ -71,18 +65,11 @@ class Pong:
             for evento in pygame.event.get():
                 if evento.type == pygame.QUIT:
                     salir = True
-                if evento.type == pygame.KEYDOWN:
-                    if evento.key == pygame.K_ESCAPE:
+                if evento.type == pygame.KEYDOWN and evento.key == pygame.K_ESCAPE:
                         salir = True
 
-
-                    
-                
-            
-
-
         
-                #Dibujo rectángulo
+#Dibujo rectángulo     
             pygame.draw.rect(self.pantalla,CBLANCO, self.jugador1)
             pygame.draw.rect(self.pantalla,CBLANCO, self.jugador2)
             
